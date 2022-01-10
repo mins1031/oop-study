71cc2b88b90098c02b7da8e62c7cd7577f2413b4
mins1031 <urisegea@naver.com>
2022-01-10 14:30:21 +0900

    oop-basic first commit

---
+++ oop-basic/src/main/java/com/example/oopbasic/OopBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopBasicApplication.class, args);
+    }
+
+}
---
+++ oop-basic/src/test/java/com/example/oopbasic/OopBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
